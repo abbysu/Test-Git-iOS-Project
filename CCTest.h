93270974cd9db6490894a5903f329667da0420ae
Susie <susie@bioit-04.local>
2016-06-09 16:23:58 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Susie on 6/9/16.
+//  Copyright © 2016 Susie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Susie on 6/9/16.
+//  Copyright © 2016 Susie. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
